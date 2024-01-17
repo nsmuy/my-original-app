887da281b2955eed9f33f16c15f9e427019efee8
yuumi <gin728emma218@gmail.com>
2024-01-17 15:34:24 +0900

    比較結果ページのUIを変更

--- src/app/comparison/result/page.tsx
+++ src/app/comparison/result/page.tsx
@@ -80,54 +80,61 @@ const ComparisonResult = () => {
   }, []);
 
   return (
-    <div>
-      <h1>比較結果</h1>
+    <div className='mt-8'>
+      <div className='inner'>
+        <h2 className='text-xl font-bold border-b border-amber-200'>比較結果</h2>
 
-      <div className='overflow-scroll'>
-        {/* writing-modeプロパティがTailWindCSSにないため, 独自のcomparisonTableクラスを作成*/}
-        <table className='comparisonTable'>
-          <thead>
-            <tr>
-              <th></th>
-              <th>ブランド</th>
-              <th>商品名</th>
-              <th>価格</th>
-              <th>SPF</th>
-              <th>色展開</th>
-              <th>容量</th>
-              <th>商品詳細</th>
-              <th>ツヤ感</th>
-              <th>カバー力</th>
-              <th>崩れにくさ</th>
-              <th>保湿力</th>
-              <th>口コミを見る</th>
-            </tr>
-          </thead>
-          <tbody>
-            {comparisonProductWithReviewsAndAverageRatings &&
-            comparisonProductWithReviewsAndAverageRatings.map(product => {
-              return (
-                <tr key={product.id}>
-                  <td><img src={product.image} alt={product.name} /></td>
-                  <td>{product.brand}</td>
-                  <td>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>{product.spf}</td>
-                  <td>{product.color}</td>
-                  <td>{product.capacity}</td>
-                  <td>{product.feature}</td>
-                  <td>{product.averageRatings.luminosity}</td>
-                  <td>{product.averageRatings.coverage}</td>
-                  <td>{product.averageRatings.longevity}</td>
-                  <td>{product.averageRatings.moisturizing}</td>
-                  <td>
-                    <button onClick={() => router.push(`/reviews/${product.id}`)}>口コミページに行く</button>
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+        <div className='overflow-scroll hide-scrollbar mt-4'>
+          {/* writing-modeプロパティがTailWindCSSにないため, 独自のcomparisonTableクラスを作成*/}
+          <table className='comparisonTable text-sm'>
+            <thead>
+              <tr>
+                <th></th>
+                <th>ブランド</th>
+                <th>商品名</th>
+                <th>価格（税込）</th>
+                <th>SPF</th>
+                <th>色展開</th>
+                <th>容量</th>
+                <th>商品詳細</th>
+                <th>ツヤ感</th>
+                <th>カバー力</th>
+                <th>崩れにくさ</th>
+                <th>保湿力</th>
+                <th>口コミを見る</th>
+              </tr>
+            </thead>
+            <tbody>
+              {comparisonProductWithReviewsAndAverageRatings &&
+              comparisonProductWithReviewsAndAverageRatings.map(product => {
+                return (
+                  <tr key={product.id}>
+                    <td><img src={product.image} alt={product.name} /></td>
+                    <td className='text-center'>{product.brand}</td>
+                    <td>{product.name}</td>
+                    <td className='text-center'>￥{product.price}</td>
+                    <td className='text-center'>{product.spf}</td>
+                    <td className='text-center'>{product.color}色</td>
+                    <td className='text-center'>{product.capacity}</td>
+                    <td>{product.feature}</td>
+                    <td className='text-center'>{product.averageRatings.luminosity || '-'}</td>
+                    <td className='text-center'>{product.averageRatings.coverage || '-'}</td>
+                    <td className='text-center'>{product.averageRatings.longevity || '-'}</td>
+                    <td className='text-center'>{product.averageRatings.moisturizing || '-'}</td>
+                    <td className='text-center'>
+                      <button
+                        onClick={() => router.push(`/reviews/${product.id}`)}
+                        className='bg-amber-200 px-4 py-1 rounded-lg hover:bg-amber-500 hover:text-white'
+                      >
+                        この商品の<br />レビューを見る
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   )
