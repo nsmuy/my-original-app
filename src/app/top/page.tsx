bf5b0a131471cc31661154d3fd5b269c9bdb968b
yuumi <gin728emma218@gmail.com>
2024-02-06 00:10:30 +0900

    TOPページのボタン遷移先変更

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -7,11 +7,11 @@ const top = () => {
       <div className='absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] right-0 bottom-0'>
         <h2 className='text-3xl text-center font-bold text-amber-500'>MY FOUNDATION FINDER へ ようこそ！</h2>
         <div className='flex gap-5 mt-8'>
-          <Link href={"/comparison"} className='bg-white rounded-md shadow-sm p-8 flex-1 text-center'>
+          <Link href={"/comparison/"} className='bg-white rounded-md shadow-sm p-8 flex-1 text-center'>
             <p>気になるファンデーションを<br/>比較する</p>
           </Link>
 
-          <Link href={"/mypage/reviews"} className='bg-white rounded-md shadow-sm p-8 flex-1 text-center'>
+          <Link href={"/reviews/"} className='bg-white rounded-md shadow-sm p-8 flex-1 text-center'>
             <p>口コミを<br/>見る・書く</p>
           </Link>
         </div>
